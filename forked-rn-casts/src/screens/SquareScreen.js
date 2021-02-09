edf71565b340a24a6a7b35311e1da00a32b43b8f
Gas <gaston.lopez@fing.edu.uy>
2021-02-09 18:31:35 -0300

    reducer

--- forked-rn-casts/src/screens/SquareScreen.js
+++ forked-rn-casts/src/screens/SquareScreen.js
@@ -1,45 +1,52 @@
-import React, {useState} from 'react';
+import React, {useReducer} from 'react';
 import { View, StyleSheet, Button, Text} from 'react-native';
 import ColorCounter from '../components/ColourCounter';
 
 const COLOR_INCREMENT = 15;
 
-const SquareScreen = () => {
-  const [blue, setBlue] = useState(0);
-  const [red, setRed] = useState(0);
-  const [green, setGreen] = useState(0);
-  console.log(`Red: ${red}`);
-  console.log(`Blue: ${blue}`);
-  console.log(`Green: ${green}`);
-
-  const setColor = (color, change) => {
-    switch(color){ 
+const reducer = (state, action)=>{
+    //state=== {red: number, green: number, blue:number }
+    //action === {colorToChange: 'red'||'green'||'blue'}
+    switch(action.colorToChange){
       case 'red':
-        red + change > 255 || red + change <0 ? null : setRed(red + change);
-        return;
-      case 'blue':
-        blue + change > 255 || blue + change <0 ? null : setBlue(blue + change)
-        return;
+        return state.red + action.amount > 255 || state.red + action.amount < 0 ?
+        state :
+        {...state, red: state.red + action.amount};
       case 'green':
-        green + change > 255 || green + change <0 ? null : setGreen(green + change)
-        return;
+        return state.green + action.amount > 255 || state.green + action.amount < 0 ?
+        state : 
+        {...state, green: state.green + action.amount};
+
+      case 'blue':
+        return state.blue + action.amount > 255 || state.blue + action.amount < 0 ?
+        state:
+        {...state, blue: state.blue + action.amount};
       default:
-        return;
-    } 
-  }
+        return state;
+    }
+}
+
+const SquareScreen = () => {
+  
+  const [state, dispatch] = useReducer(reducer, {red: 0, green: 0, blue: 0});
+  const {red, green, blue} = state;
+  console.log(state);//{red: 0, green: 0, blue:0}
+
+
+
   return (
   <View>
       <ColorCounter 
-        onIncrease={()=> {setColor('red', COLOR_INCREMENT)}} 
-        onDecrease={()=> setColor('red' , -1 * COLOR_INCREMENT)} 
+        onIncrease={()=> dispatch({colorToChange:'red', amount: COLOR_INCREMENT})}
+        onDecrease={()=> dispatch({colorToChange:'red', amount: -1* COLOR_INCREMENT})} 
         color="red"/>
       <ColorCounter 
-        onIncrease={()=> setColor('blue', COLOR_INCREMENT)} 
-        onDecrease={()=> setColor('blue', -1 * COLOR_INCREMENT)} 
+        onIncrease={()=> dispatch({colorToChange:'blue', amount: COLOR_INCREMENT})} 
+        onDecrease={()=> dispatch({colorToChange:'blue', amount: -1* COLOR_INCREMENT})} 
         color="blue"/>
       <ColorCounter 
-        onIncrease={()=> setColor('green', COLOR_INCREMENT)} 
-        onDecrease={()=> setColor('green', -1 * COLOR_INCREMENT)} 
+        onIncrease={()=> dispatch({colorToChange:'green', amount: COLOR_INCREMENT})} 
+        onDecrease={()=> dispatch({colorToChange:'green', amount: -1* COLOR_INCREMENT})} 
         color="green"/>
       <View style={{height: 100, width: 100, backgroundColor: `rgb(${red},${green},${blue})`}}/>
   </View>
